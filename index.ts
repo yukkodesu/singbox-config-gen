a4a47e72eedca8225c4becf32b0050225318572e
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2025-03-01 22:55:07 +0800

    feat: add support for clash subscription which has obfs plugin

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@ import { readFileToJSON, writeJSONToFile } from "./src/utils";
 import type { SingboxConfig } from "./src/types";
 
 const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const subUrl = await rl.question("Input your subscription:\n");
@@ -15,42 +15,58 @@ rl.close();
 const sub = await (await fetch(subUrl)).text();
 const doc = yaml.load(sub);
 
-const base_config = await readFileToJSON<SingboxConfig>("base_configs/base_config.json");
+const base_config = await readFileToJSON<SingboxConfig>(
+  "base_configs/base_config.json"
+);
 const tun = await readFileToJSON("base_configs/tun.json");
 const proxy_names: string[] = [];
-const outbounds = doc.proxies.map((it: {
+const outbounds = doc.proxies.map(
+  (it: {
     name: string;
     type: string;
     server: string;
     port: string;
     cipher: string;
     password: string;
-}) => {
-    proxy_names.push(it.name)
+    udp?: boolean;
+    plugin?: string;
+    "plugin-opts"?: Record<string, string>;
+  }) => {
+    proxy_names.push(it.name);
     return {
-        "type": it.type === "ss" ? "shadowsocks" : it.type,
-        "server": it.server,
-        "server_port": it.port,
-        "method": it.cipher,
-        "password": it.password,
-        "tag": it.name
-    }
-});
+      type: it.type === "ss" ? "shadowsocks" : it.type,
+      server: it.server,
+      server_port: it.port,
+      method: it.cipher,
+      password: it.password,
+      plugin: it.plugin?.includes("obfs") ? "obfs-local" : undefined,
+      plugin_opts:
+        it.plugin && it.plugin.includes("obfs")
+          ? `obfs=${it["plugin-opts"]?.mode};obfs-host=${it["plugin-opts"]?.host}`
+          : undefined,
+      tag: it.name,
+    };
+  }
+);
 
 for (const i of base_config.route.rule_set) {
-    i.download_detour = proxy_names[3];
+  i.download_detour = proxy_names[3];
 }
 const pc_config = cloneDeep(base_config);
 pc_config.outbounds = [...base_config.outbounds].concat(outbounds);
-let select = pc_config.outbounds.find((it: { tag: string; }) => it.tag === "proxy");
+let select = pc_config.outbounds.find(
+  (it: { tag: string }) => it.tag === "proxy"
+);
 if (select) {
-    select.outbounds = [...proxy_names];
-    select.default = proxy_names[20];
+  select.outbounds = [...proxy_names];
+  select.default = proxy_names[20];
 }
 
 const mobile_config = cloneDeep(pc_config);
 mobile_config.inbounds = [tun];
-select = mobile_config.outbounds.find((it: { tag: string; }) => it.tag === "proxy");
+select = mobile_config.outbounds.find(
+  (it: { tag: string }) => it.tag === "proxy"
+);
 select && delete select.default;
 
 writeJSONToFile("output/pc_config.json", pc_config);
