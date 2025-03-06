98bb24efdb9ee5df46e1669340638617c3f82d9c
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2025-03-06 23:42:16 +0800

    chore: format code

--- src/utils.ts
+++ src/utils.ts
@@ -2,18 +2,17 @@ import { existsSync, mkdirSync } from "fs";
 import { readFile, writeFile } from "fs/promises";
 import path from "path";
 
-
-export const readFileToJSON = async<T>(path: string): Promise<T> => {
-    return JSON.parse(await readFile(path, { encoding: 'utf-8' }));
-}
+export const readFileToJSON = async <T>(path: string): Promise<T> => {
+  return JSON.parse(await readFile(path, { encoding: "utf-8" }));
+};
 
 export const writeJSONToFile = async (path: string, obj: any) => {
-    await writeFile(path, JSON.stringify(obj));
-}
+  await writeFile(path, JSON.stringify(obj));
+};
 
 export function safeMkdir(filePath: string) {
-    if (existsSync(filePath)) {
-      return;
-    }
-    mkdirSync(filePath, { recursive: true });
+  if (existsSync(filePath)) {
+    return;
   }
+  mkdirSync(filePath, { recursive: true });
+}
