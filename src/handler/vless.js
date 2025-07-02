cf2fa1447066ee3d435dea98d7d6e7255fd8db03
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2025-07-02 15:43:29 +0800

    feat: add initial support for vless

---
+++ src/handler/vless.js
@@ -0,0 +1,24 @@
+export const vlessHandler = (clashProxy) => {
+  const it = clashProxy;
+  const reality = it["reality-opts"];
+  return {
+    type: "vless",
+    tag: it.name,
+    server: it.server,
+    server_port: Number(it.port),
+    uuid: it.uuid,
+    flow: it.flow,
+    tls: {
+      enabled: !!it.tls,
+      server_name: it.servername,
+      reality: {
+        public_key: reality["public-key"],
+        short_id: reality["short-id"],
+      },
+    },
+    utls: {
+      enabled: !!it["client-fingerprint"],
+      fingerprint: it["client-fingerprint"],
+    },
+  };
+};
